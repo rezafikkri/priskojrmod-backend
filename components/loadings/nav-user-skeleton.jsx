27035e2def1dfa654f66f4ecab95c96caf094ca8
rezafikkri <fikkri.reza@gmail.com>
2025-03-30 12:23:21 +0700

    feat: improve nav-user skeleton height

--- components/loadings/nav-user-skeleton.jsx
+++ components/loadings/nav-user-skeleton.jsx
@@ -5,8 +5,8 @@ export default function NavUserSkeleton() {
     <div className="flex items-center space-x-2">
       <Skeleton className="size-8 rounded-full bg-zinc-300/50" />
       <div className="space-y-1.5 flex-1">
-        <Skeleton className="h-4 w-full bg-zinc-300/50 rounded-sm" />
-        <Skeleton className="h-3 w-1/2 bg-zinc-300/50 rounded-sm" />
+        <Skeleton className="h-4.5 w-full bg-zinc-300/50 rounded-md" />
+        <Skeleton className="h-4 w-1/2 bg-zinc-300/50 rounded-md" />
       </div>
     </div>
   );
