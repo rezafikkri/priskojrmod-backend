d09f179087a173a89a0b89ad5137f51115d4a9d0
rezafikkri <fikkri.reza@gmail.com>
2025-04-26 08:01:15 +0700

    test: fix error in secret key create and delete

--- tests/lib/services/secret-key-service.test.js
+++ tests/lib/services/secret-key-service.test.js
@@ -54,12 +54,19 @@ describe('createSecretKey function', () => {
     const pjmaDBPrismaClient = (await import('@/lib/pjma-prisma-client')).default;
 
     verifySession.mockResolvedValue({ isAuth: true, userId: '123' });
+    pjmaDBPrismaClient.SecretKeyLicense.create.mockResolvedValue({
+      id: 1n,
+      created_at: 123456789n,
+    });
 
-    await createSecretKey({ key: 'a9T7vX3kLp2WzB6dQe8MfYrJc0UhNs4g', app_name: 'test-app' });
+    await createSecretKey({
+      key: '8f23fcc4c918eb26c991b3950c79a243a6b0d683c2e58e0d31fc367b652e2b05',
+      app_name: 'test-app',
+    });
 
     expect(pjmaDBPrismaClient.SecretKeyLicense.create).toHaveBeenCalledWith({
       data: {
-        key: 'a9T7vX3kLp2WzB6dQe8MfYrJc0UhNs4g',
+        key: '8f23fcc4c918eb26c991b3950c79a243a6b0d683c2e58e0d31fc367b652e2b05',
         app_name: 'test-app',
         product_id: 'uuidv7',
         created_at: BigInt(Math.floor(new Date().getTime() / 1000)),
@@ -88,11 +95,17 @@ describe('deleteSecretKey function', () => {
     const pjmaDBPrismaClient = (await import('@/lib/pjma-prisma-client')).default;
 
     verifySession.mockResolvedValue({ isAuth: true, userId: '123' });
+    pjmaDBPrismaClient.SecretKeyLicense.delete.mockResolvedValue({
+      id: 2n,
+    });
 
     await deleteSecretKey('1');
 
     expect(pjmaDBPrismaClient.SecretKeyLicense.delete).toHaveBeenCalledWith({
       where: { id: BigInt(1) },
+      select: {
+        id: true,
+      },
     });
   });
 });
