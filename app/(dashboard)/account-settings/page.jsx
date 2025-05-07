6a8f55a5c3679e6c39a534617d12cc2e78ce5647
rezafikkri <fikkri.reza@gmail.com>
2025-05-07 19:18:09 +0700

    feat: add skeleton loading in account settings form

--- app/(dashboard)/account-settings/page.jsx
+++ app/(dashboard)/account-settings/page.jsx
@@ -1,4 +1,5 @@
 import AccountSettingsForm from "@/components/account-settings/account-settings-form";
+import FormSkeleton from "@/components/loadings/form-skeleton";
 import { Suspense } from "react";
 
 export const metadata = {
@@ -9,7 +10,7 @@ export default function AccountSettings() {
   return (
     <>
       <h1 className="text-xl mb-7 font-bold">Account Settings</h1>
-      <Suspense fallback={'Loading...'}>
+      <Suspense fallback={<FormSkeleton />}>
         <AccountSettingsForm />
       </Suspense>
     </>
