61adb1017bd0d74f80e00b5b4bed6408017b41d7
rezafikkri <fikkri.reza@gmail.com>
2025-05-30 10:16:46 +0700

    feat: add form-fields component for category

--- components/category/create-form.jsx
+++ components/category/create-form.jsx
@@ -2,22 +2,10 @@
 
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
-import { Button } from '@/components/ui/button';
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
-import { Loader2, ArrowLeft } from 'lucide-react';
 import { toast } from 'sonner';
-import Link from 'next/link';
 import { categorySchema } from '@/lib/validators/category-validator';
 import { addCategory } from '@/actions/category-actions';
+import FormFields from './form-fields';
 
 export default function CreateForm() {
   const form = useForm({
@@ -26,7 +14,6 @@ export default function CreateForm() {
       name: '',
     },
   });
-  const isSubmitting = form.formState.isSubmitting;
 
   async function handleSubmit(data) {
     const addRes = await addCategory(data);
@@ -38,41 +25,5 @@ export default function CreateForm() {
     }
   }
 
-  return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-6 lg:max-w-2/3 mb-10">
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel className="text-base">Name</FormLabel>
-              <FormControl>
-                <Input disabled={isSubmitting} {...field} className="md:text-base h-auto px-3 py-1.5 shadow-none" />
-              </FormControl>
-              <FormDescription>Enter the category name.</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button asChild variant="outline" className="me-3 mb-0 h-auto inline-block text-base px-3 py-1.5">
-          <Link href="/category"><ArrowLeft className="icon" /> Back</Link>
-        </Button>
-        <div className="relative inline-block">
-          <Button
-            type="submit"
-            className={`h-auto text-base px-3 py-1.5 disabled:opacity-100 ${isSubmitting ? 'transition-none' : ''} border border-primary`}
-            disabled={isSubmitting}
-          >
-            <span className={isSubmitting ? 'opacity-0' : ''}>Create</span>
-          </Button>
-          {isSubmitting && (
-            <div className="absolute h-full top-0 left-0 right-0 flex justify-center items-center">
-              <Loader2 className="animate-spin text-primary-foreground" size={16} />
-            </div>
-          )}
-        </div>
-      </form>
-    </Form>
-  );
+  return <FormFields mode="create" form={form} onSubmit={handleSubmit} />
 }
